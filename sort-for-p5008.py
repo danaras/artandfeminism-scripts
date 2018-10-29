ddb6c833613000c0c119206fcc10c27e99c24178
Danara Sarioglu <ds4489@nyu.edu>
2018-10-29 14:16:42 -0400

    updated grey list output

--- sort-for-p5008.py
+++ sort-for-p5008.py
@@ -1,18 +1,25 @@
 import csv
 
-inputFile = 'all-categories-QS.csv'
-qidColumnNumber = 0
-occupationQIDColumnNumber = 2
+inputFile = ''
+qidColumnNumber = 2 #write the column number where qid is (starting with 0, 1, 2....)
+occupationQIDColumnNumber = 5 #write the column number where occupation qid is (starting with 0, 1, 2....)
 listFile = 'Resources/red-green-list.csv'
 outputFile_green = 'green-' + inputFile
 outputFile_red = 'red-' + inputFile
 outputFile_grey = 'grey-' + inputFile
 
-def createOutputCsv (outputFileName):
+def createOutputCsv(outputFileName, listColor, delimeterValue):
 	outputCSV = open(outputFileName, 'w')
-	csvWriter = csv.writer(outputCSV, delimiter='	')
-	csvWriter.writerow(['QID'])
+	csvWriter = csv.writer(outputCSV, delimiter=delimeterValue)
+	if listColor == 'green':
+		csvWriter.writerow(['QID','ON FOCUS LIST','ART+FEMINISM QID'])
+	elif listColor == 'red':
+		csvWriter.writerow(['QID'])
+	elif listColor == 'grey':
+		csvWriter.writerow(['language','title','QID','P21','gender','P106','occupation','WP first sentence'])
 	return [outputCSV,csvWriter]
+
+
 def getRedGreenList(listFileName):
 	sortList = []
 	with open(listFileName,'r') as csvFile:
@@ -35,7 +42,7 @@ def writeOut(sorted, outputs):
 		outputs[1][1].writerow([qid])
 		outputs[1][0].flush()
 	for qid in sorted[2]:
-		outputs[2][1].writerow([qid])
+		outputs[2][1].writerow(qid)
 		outputs[2][0].flush()
 
 def parseCsv(input, sortList, qidColumnNo, occupationQIDColumnNo):
@@ -46,6 +53,7 @@ def parseCsv(input, sortList, qidColumnNo, occupationQIDColumnNo):
 		firstline = True
 		reader = csv.reader(csvFile)
 		for row in reader:
+			found = False
 			if firstline:    #skip first line
 				firstline = False
 				continue
@@ -54,25 +62,33 @@ def parseCsv(input, sortList, qidColumnNo, occupationQIDColumnNo):
 				if x[1] == info[occupationQIDColumnNumber]:
 					if x[3] == 'y':	#add qid to green qid list
 						greenQIDs.append(info[qidColumnNumber])
+						found = True
+						break
 					elif x[3] == 'n':	#add qid to red qid list
 						redQIDs.append(info[qidColumnNumber])
+						found = True
+						break
 					else:	#add qid to grey qid list
-						greyQIDs.append(info[qidColumnNumber])
-				else:	#add qid to grey qid list
-					greyQIDs.append(info[qidColumnNumber])
+						greyQIDs.append(info)
+						found = True
+						break
+					#add qid to grey qid list
+			if found == False:
+				greyQIDs.append(info)
+					# break
 		# SORT THE LISTS (remove duplicates etc.)
 		greenQIDs = list(set(greenQIDs))
 		redQIDs = list(set(redQIDs) - set(greenQIDs))
-		greyQIDs = list(set(greyQIDs) - set(greenQIDs) - set(redQIDs))
+		# greyQIDs = set(greyQIDs)
 		return [greenQIDs,redQIDs,greyQIDs]
 
 def main():
 	print "Start"
 	outputFiles  = []
 	red_green_list = getRedGreenList(listFile)
-	outputFiles.append(createOutputCsv(outputFile_green))
-	outputFiles.append(createOutputCsv(outputFile_red))
-	outputFiles.append(createOutputCsv(outputFile_grey))
+	outputFiles.append(createOutputCsv(outputFile_green,'green','	'))
+	outputFiles.append(createOutputCsv(outputFile_red,'red','	'))
+	outputFiles.append(createOutputCsv(outputFile_grey,'grey',','))
 	sortedList = parseCsv(inputFile, red_green_list, qidColumnNumber, occupationQIDColumnNumber)
 	writeOut(sortedList, outputFiles)
 
